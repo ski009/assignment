a86731a9c6960fb87ddf5a8a828c29a7f4724fea
Mridul <mr.mridulshukla@gmail.com>
2025-03-03 19:09:50 +0530

    adding test

--- assignment.rb
+++ assignment.rb
@@ -21,3 +21,34 @@
     numbers.sum
 end
 
+class TestSuperDuperMethod < Test::Unit::TestCase
+    def test_valid_input_with_custom_delimiter
+        input_sting = "//;\n1;2;3;5\n"
+        result = processing_string_to_add(input_sting)
+        assert_equal(11, result, "The sum of the numbers should be 11")
+    end
+
+    def test_input_with_negative_number
+        input_sting = "//;\n1;2;3;-4;5\n"
+        assert_raises(RuntimeError, "Negative numbers are not allowed: -4") do
+            processing_string_to_add(input_sting)
+        end
+    end
+
+    def test_empty_input
+        input_sting = "//;\n\n"
+        result = processing_string_to_add(input_sting)
+        assert_equal(0, result, "The sum of an empty input_sting should be 0")
+    end
+
+    def test_custom_delimiter_with_multiple_numbers
+        input_sting = "//|\n10|20|30|40|50\n"
+        result = processing_string_to_add(input_sting)
+        assert_equal(150, result, "The sum of the numbers should be 150")
+    end
+end
+
+    # Run the tests
+if __FILE__ == $0
+    Test::Unit::AutoRunner.run
+end
